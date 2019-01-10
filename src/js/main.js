023b3c098252821c84cecf157917c1d4798713d7
Sergey Khmelevskoy <xs290@me.com>
2019-01-10 20:44:35 +0300

    contacts

--- src/js/main.js
+++ src/js/main.js
@@ -88,6 +88,7 @@ $(document).ready(function(){
     initRangeSlider();
     initScrollMonitor(fromPjax);
     initValidations();
+    initMaps();
   }
 
   // The transition has just finished and the old Container has been removed from the DOM.
@@ -1906,6 +1907,47 @@ $(document).ready(function(){
       setTimeout(function(){
         $('.dev-bp-debug').remove();
       },1500)
+    }
+  }
+
+  //////////
+  // MAPS
+  //////////
+  function initMaps(){
+    var $map = $('[js-contact-map]');
+    if ( $map.length === 0 ) return
+    // When the window has finished loading create our google map below
+    google.maps.event.addDomListener(window, 'load', init);
+
+    function init() {
+      var mapOptions = {
+        zoom: 10,
+        disableDefaultUI: true,
+        center: new google.maps.LatLng(55.753215, 37.622504), // Moscow
+        styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
+      };
+
+      var mapElement = $map.get(0)
+      // Create the Google Map using our element and options defined above
+      var map = new google.maps.Map(mapElement, mapOptions);
+      var icon = {
+        scaledSize: new google.maps.Size(105, 48),
+        url: 'img/template/map-marker@2x.png'
+      }
+      new google.maps.Marker({
+        position: new google.maps.LatLng(55.722563, 37.612668),
+        map: map,
+        title: 'Демонстрационный зал',
+        icon: icon
+      });
+
+      new google.maps.Marker({
+        position: new google.maps.LatLng(55.812024, 37.833994),
+        map: map,
+        title: 'Склад',
+        icon: icon
+      });
+
     }
   }
 
