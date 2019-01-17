d398cb6e7c73d62364a0ae06ca6844f40740e6c1
Sergey Khmelevskoy <xs290@me.com>
2019-01-17 20:07:34 +0300

    contacts responsive

--- src/js/main.js
+++ src/js/main.js
@@ -101,7 +101,6 @@ $(document).ready(function(){
 
     initResponsiveSliders();
     initSliders();
-
     initPopups();
     initSticky();
     initMasks();
@@ -109,6 +108,7 @@ $(document).ready(function(){
     initRangeSlider();
     initScrollMonitor(fromPjax);
     initValidations();
+    initTeleport();
   }
 
   // The transition has just finished and the old Container has been removed from the DOM.
@@ -2125,6 +2125,43 @@ $(document).ready(function(){
   function triggerBody(){
     $(window).scroll();
     $(window).resize();
+  }
+
+  ////////////
+  // TELEPORT PLUGIN
+  ////////////
+  function initTeleport(){
+    $('[js-teleport]').each(function (i, val) {
+      var self = $(val)
+      var objHtml = $(val).html();
+      var target = $('[data-teleport-target=' + $(val).data('teleport-to') + ']');
+      var conditionMedia = $(val).data('teleport-condition').substring(1);
+      var conditionPosition = $(val).data('teleport-condition').substring(0, 1);
+
+      if (target && objHtml && conditionPosition) {
+
+        function teleport() {
+          var condition;
+
+          if (conditionPosition === "<") {
+            condition = _window.width() < conditionMedia;
+          } else if (conditionPosition === ">") {
+            condition = _window.width() > conditionMedia;
+          }
+
+          if (condition) {
+            target.html(objHtml)
+            self.html('')
+          } else {
+            self.html(objHtml)
+            target.html("")
+          }
+        }
+
+        teleport();
+        _window.on('resize', debounce(teleport, 100));
+      }
+    })
   }
 
   //////////
